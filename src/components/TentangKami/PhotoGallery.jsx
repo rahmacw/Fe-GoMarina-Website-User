02c368ce70514e304095cfeb9b05b8cedd8b0c70
rahmacw <150320121+rahmacw@users.noreply.github.com>
2024-12-19 05:43:54 +0700

    Update PhotoGallery.jsx

--- src/components/TentangKami/PhotoGallery.jsx
+++ src/components/TentangKami/PhotoGallery.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 
+// Import gambar
+import spotFoto03 from "../../assets/images/tentang-kami/spot-foto03.jpg";
+import jambu03 from "../../assets/images/tentang-kami/jambu03.jpg";
+import jambuMerah03 from "../../assets/images/tentang-kami/jambu-merah03.jpg";
+import berkuda01 from "../../assets/images/tentang-kami/berkuda01.jpg";
+
 const PhotoGallery = () => (
   <div className="photo-gallery2">
     <div className="photo-box2">
-      <img src="src/assets/images/tentang-kami/spot-foto03.jpg" alt="Photo 1" />
+      <img src={spotFoto03} alt="Photo 1" />
     </div>
     <div className="photo-box2">
-      <img src="src/assets/images/tentang-kami/jambu03.jpg" alt="Photo 2" />
+      <img src={jambu03} alt="Photo 2" />
     </div>
     <div className="photo-box2">
-      <img
-        src="src/assets/images/tentang-kami/jambu-merah03.jpg"
-        alt="Photo 3"
-      />
+      <img src={jambuMerah03} alt="Photo 3" />
     </div>
     <div className="photo-box2">
-      <img src="src/assets/images/tentang-kami/berkuda01.jpg" alt="Photo 4" />
+      <img src={berkuda01} alt="Photo 4" />
     </div>
   </div>
 );
