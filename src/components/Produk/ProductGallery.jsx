2cf67fadaeb435fc4e7a7c2beb4d5e8029321623
rahmacw <150320121+rahmacw@users.noreply.github.com>
2024-12-19 05:36:48 +0700

    Update ProductGallery.jsx

--- src/components/Produk/ProductGallery.jsx
+++ src/components/Produk/ProductGallery.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 
+// Import gambar
+import hampersImage from '../../assets/images/produk/hampers01.jpg';
+import selaiJambuImage from '../../assets/images/produk/selaijambu01.jpg';
+import kripikJambuImage from '../../assets/images/produk/kripikjambu01.webp';
+import jusJambuImage from '../../assets/images/produk/jusjambu01.jpg';
+
 const ProductGallery = () => {
   return (
     <div className="photo-gallery2">
       <div className="photo-box2">
-        <img src="src/assets/images/produk/hampers01.jpg" alt="Photo 1" />
+        <img src={hampersImage} alt="Photo 1" />
       </div>
       <div className="photo-box2">
-        <img src="src/assets/images/produk/selaijambu01.jpg" alt="Photo 2" />
+        <img src={selaiJambuImage} alt="Photo 2" />
       </div>
       <div className="photo-box2">
-        <img src="src/assets/images/produk/kripikjambu01.webp" alt="Photo 3" />
+        <img src={kripikJambuImage} alt="Photo 3" />
       </div>
       <div className="photo-box2">
-        <img src="src/assets/images/produk/jusjambu01.jpg" alt="Photo 4" />
+        <img src={jusJambuImage} alt="Photo 4" />
       </div>
     </div>
   );
