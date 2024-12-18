3a8ee91b513b5de3576da1357bfc298d75d7918f
rahmacw <150320121+rahmacw@users.noreply.github.com>
2024-12-19 05:46:02 +0700

    Update TKSlogan.jsx

--- src/components/TentangKami/TKSlogan.jsx
+++ src/components/TentangKami/TKSlogan.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+// Import gambar
+import guavaBg from "../../assets/images/tentang-kami/slogan-tk/guavabg.png";
+import sloganTk01 from "../../assets/images/tentang-kami/slogan-tk/slogan-tk01.jpg";
+
 function TKSlogan() {
   return (
     <div className="tkslogan-container">
@@ -10,7 +14,7 @@ function TKSlogan() {
         <h2 style={{ paddingLeft: "93%" }}>"</h2>
         <div className="tkimage-left">
           <img
-            src="src/assets/images/tentang-kami/slogan-tk/guavabg.png"
+            src={guavaBg}
             alt="Foto kebun"
             className="tkslogan-photo1"
           />
@@ -18,7 +22,7 @@ function TKSlogan() {
       </div>
       <div className="tkslogan-right">
         <img
-          src="src/assets/images/tentang-kami/slogan-tk/slogan-tk01.jpg"
+          src={sloganTk01}
           alt="Foto kebun"
           className="tkslogan-photo2"
         />
