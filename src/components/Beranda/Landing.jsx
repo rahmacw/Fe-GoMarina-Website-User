98c9689e0a24d7d0dbcc4672a4fcc6acea39987a
rahmacw <150320121+rahmacw@users.noreply.github.com>
2024-12-18 21:53:26 +0700

    Kembali Landing.jsx

--- src/components/Beranda/Landing.jsx
+++ src/components/Beranda/Landing.jsx
@@ -1,22 +1,17 @@
-import React from "react";
+import React from 'react';
+
 
 const Landing = () => (
   <section className="landing">
     <div className="garden-beauty">
       <img
         src="src/assets/images/beranda/keindahan-kebun/keindahan-kebun3.jpeg"
-        alt="Keindahan pemandangan kebun kami yang asri dan menenangkan"
+        alt="Keindahan Kebun"
         className="garden-image"
-        onError={(e) => {
-          e.target.onerror = null; // Mencegah loop error
-          e.target.src = "src/assets/images/default-image.jpeg"; // Fallback gambar
-        }}
       />
       <h2>Selamat Datang di Kebun Kami!</h2>
       <p>Batam, Indonesia</p>
-      <a href="#explore" className="explore-button">
-        Jelajahi
-      </a>
+      <a href="#" className="explore-button">Jelajahi</a>
     </div>
   </section>
 );
