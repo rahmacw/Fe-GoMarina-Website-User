26defd5f0e1b9452fce193d17d5c265a1dfdd1ac
rahmacw <150320121+rahmacw@users.noreply.github.com>
2024-12-19 05:41:28 +0700

    Update ManagementTeam.jsx

--- src/components/TentangKami/ManagementTeam.jsx
+++ src/components/TentangKami/ManagementTeam.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 
+// Import gambar
+import image01 from '../../assets/images/tentang-kami/tim-pengelola/image01.jpeg';
+import image02 from '../../assets/images/tentang-kami/tim-pengelola/image02.jpeg';
+import image03 from '../../assets/images/tentang-kami/tim-pengelola/image03.jpeg';
+
 function ManagementTeam() {
   const teamMembers = [
     {
       name: "Dr. Budi Santoso",
       role: "Manajer Kebun",
-      imgSrc: "src/assets/images/tentang-kami/tim-pengelola/image01.jpeg",
+      imgSrc: image01, // Gambar Dr. Budi Santoso
     },
     {
       name: "Ibu Siti Aminah",
       role: "Asisten Manajer Kebun",
-      imgSrc: "src/assets/images/tentang-kami/tim-pengelola/image02.jpeg", // Separate image for Ibu Siti
+      imgSrc: image02, // Gambar Ibu Siti Aminah
     },
     {
       name: "Ibu Suminah Widodo",
       role: "Pengawas Kebun",
-      imgSrc: "src/assets/images/tentang-kami/tim-pengelola/image03.jpeg", // Separate image for Bapak Joko
+      imgSrc: image03, // Gambar Ibu Suminah Widodo
     },
   ];
 
@@ -26,7 +31,7 @@ function ManagementTeam() {
         {teamMembers.map((member, index) => (
           <div className="team-member" key={index}>
             <img
-              src={member.imgSrc} // Using separate image for each member
+              src={member.imgSrc} // Menggunakan variabel imgSrc
               alt={member.name}
             />
             <h4>{member.name}</h4>
