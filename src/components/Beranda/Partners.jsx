ace27a297195115e9d07a77988baf59ac4117d55
rahmacw <150320121+rahmacw@users.noreply.github.com>
2024-12-18 22:30:05 +0700

    Update Partners.jsx

--- src/components/Beranda/Partners.jsx
+++ src/components/Beranda/Partners.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
+import mitra01 from '../../assets/images/beranda/mitra-kerjasama/mitra00.jpg'; // Mengimpor gambar
 
 const Partners = () => (
   <div className="partners">
-        <h3>Mitra Kerjasama</h3>
-        <div className="partner-list">
-          <div className="partner">
-            <img src="src/assets/images/beranda/mitra-kerjasama/mitra00.jpg" alt="Partner 1" className="partner-image" />
-            <p>Partner 1</p>
-          </div>
-          <div className="partner">
-            <img src="src/assets/images/beranda/mitra-kerjasama/mitra00.jpg" alt="Partner 2" className="partner-image" />
-            <p>Partner 2</p>
-          </div>
-          <div className="partner">
-            <img src="src/assets/images/beranda/mitra-kerjasama/mitra00.jpg" alt="Partner 3" className="partner-image" />
-            <p>Partner 3</p>
-          </div>
-          <div className="partner">
-            <img src="src/assets/images/beranda/mitra-kerjasama/mitra00.jpg" alt="Partner 4" className="partner-image" />
-            <p>Partner 4</p>
-          </div>
-        </div>
+    <h3>Mitra Kerjasama</h3>
+    <div className="partner-list">
+      <div className="partner">
+        <img src={mitra01} alt="Partner 1" className="partner-image" />
+        <p>Partner 1</p>
       </div>
+      <div className="partner">
+        <img src={mitra01} alt="Partner 2" className="partner-image" />
+        <p>Partner 2</p>
+      </div>
+      <div className="partner">
+        <img src={mitra01} alt="Partner 3" className="partner-image" />
+        <p>Partner 3</p>
+      </div>
+      <div className="partner">
+        <img src={mitra01} alt="Partner 4" className="partner-image" />
+        <p>Partner 4</p>
+      </div>
+    </div>
+  </div>
 );
 
 export default Partners;
